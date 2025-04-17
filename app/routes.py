6c5e66eb64701ed4ad36e1eec707c7ab5c15bc00
Старшинов Фёдор Андреевич <feanstarshinov@edu.hse.ru>
2025-04-18 00:20:21 +0300

    routes.py apdated (added upload blueprint)

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import render_template
 from flask_login import LoginManager, login_required, current_user
 from models import Users
 from auth.routes import auth
+from upload.routes import upload
 
 from app import app
 
@@ -9,6 +10,8 @@ login_manager = LoginManager(app)
 login_manager.login_view = "auth.login"
 
 app.register_blueprint(auth, url_prefix="/auth")
+app.register_blueprint(upload, url_prefix="/upload")
+
 
 @login_manager.user_loader
 def load_user(user_id):
